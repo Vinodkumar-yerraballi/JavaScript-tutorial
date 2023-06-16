61dea3004c051ee30e03638bc960b4eafb37590a
unknown <vinodkumar9676150@gmail.com>
2023-06-16 12:07:25 +0530

    first commit

---
+++ Fundamental/data_types.js
@@ -0,0 +1,48 @@
+let number=8   // It should be a number dtypes
+let user = "Vindo kuma" // it is a string dtypes
+let num1=30393030303910199230
+console.log(num1*3.15)
+// checking the type of the data type
+console.log(typeof num1)
+console.log(typeof user)
+// Helxagonal numeber in the data types
+let num2 =0xf
+console.log(num2)
+// explonental numeber in the data types
+let num3 =1.5e12
+console.log(num3)   //15*10*2
+let num4 =5/0
+console.log(num4) // we get positive infinity
+let num5 =-5/0
+console.log(num5) // we get negative infinity
+
+console.log(Number.MAX_VALUE*20)  // max value for the number multiplied by 20 we get positive infinity
+
+// Bign means to print the entered value in the variable
+let entired_num =19192949292839283928n
+console.log(entired_num); // when we enter n in the end of the nummber we print entire values without mistakes
+// we can add the another number to the entired_num
+console.log(entired_num + 5n) //we add the 5 to the entired_num values we get the number of values
+// Concatenate the two string values together
+let firstName ="Vinod kumar"
+let lastName ="Yerraballi"
+let userName_of_myname=firstName+" "+lastName
+console.log(userName_of_myname)
+
+// double codes in the text
+let sampleName="Vinod Kumar \"is a Data Scientist\""
+console.log(sampleName)
+// boolean types in the javascript code
+let bool1=5<6
+let bool2=5>6
+console.log(bool1)
+console.log(bool2)
+console.log(typeof bool1)
+console.log(typeof bool2)
+//  null types in javascript
+let nullValues=null
+console.log(nullValues)  // we get the output is null
+console.log(typeof nullValues)  // we get the output is object
+// wihout given the values
+let user_values
+console.log(user_values) // undefined
---
+++ Fundamental/hello.js
@@ -0,0 +1,42 @@
+// Bascis of the JavaScript
+console.log("Hello world!");
+// Variables of the JavaScript
+a=2
+b=3
+num = 2+2+3
+console.log(num +3)
+console.log(num+5)
+console.log(a+b)
+// create a new variable we can use the let function at begning of the  variable
+// Once we declare the variable we can't use same variable to other variab elements
+let number=4+4
+console.log("The addititon of the three numbers ",number+5)
+let username="Vinod Kumar"
+let greeting='Hello I hope you doing well'
+console.log(username+greeting)
+// you can use underscore notation in the variable name. "_ " and "$" are only used in the variable name 
+//  You can also use numers in the variable name but not first in the variable name it should be last in the variable name
+let user_name="Vinod Kumar"
+console.log(user_name)
+let $username_of_employe="Jhon"
+let username1="Virat"
+// Camel method is in the variabel second name start with Capital letter
+let userName="King Kohli is the greatest batsman"
+console.log($username_of_employe)
+console.log(userName)
+console.log(username1)
+
+// Store the data inthe variable  
+// find the circuler radius
+let radius=5
+let pi=3.14
+let area
+area=pi *radius*radius
+console.log(area)
+// when the variable is constant we can't change it we use keyword const
+const pie=3.14
+let radius1=6
+let area1
+area1=pie*radius1*radius1
+console.log(area1)
+
